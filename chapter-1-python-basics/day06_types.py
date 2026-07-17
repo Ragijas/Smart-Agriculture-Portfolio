86d7fa882debc0dab8214ed211bf0c0f5ef1d251
Ragijas <anilsoydan94@gmail.com>
2026-07-17 12:20:54 +0300

    feat: day 6 - variables and data types

---
+++ chapter-1-python-basics/day06_types.py
@@ -0,0 +1,8 @@
+temperature = 24
+humidity = 65.5
+sensor_name = "DHT22"
+is_online = True
+print(f"Sensor: {sensor_name}")
+print(f"Temperature: {temperature} ({type(temperature)})")
+print(f"Humidity: {humidity} ({type(humidity)})")
+print(f"Is Online: {is_online} ({type(is_online)})")
